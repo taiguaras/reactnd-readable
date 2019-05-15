a5e72775efe6ee993fc4fd28dcab90ca0ca70231
taiguaras <taiguaras@gmail.com>
2019-05-14 22:38:00 -0300

    Bootstrap dependencies

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {Card, Button, Img} from 'bootstrap';
 class Main extends Component {
 
  
@@ -9,7 +9,15 @@ render() {
     return (
       <div className="main-page" >
         <p>Mounted Main</p>
-        
+
+        <div class="card" style={{width: '18rem'}}>         
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+                
       </div>
     );
 
